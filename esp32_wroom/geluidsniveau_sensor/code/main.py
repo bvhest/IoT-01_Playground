476f9966dc4f9b217f3a65c18a20605dc91543be
B van Hest <hestbv@gmail.com>
2019-06-14 22:39:29 +0200

    Code klaar voor LED vu-meter. Nu nog voeding.

--- esp32_wroom/geluidsniveau_sensor/code/main.py
+++ esp32_wroom/geluidsniveau_sensor/code/main.py
@@ -2,26 +2,19 @@ from machine import Pin, ADC
 import time
 
 print("Initialiseer ADC: adc2, ch0")
+# see
+#   - https://docs.micropython.org/en/latest/esp32/quickref.html
+#   - https://github.com/loboris/MicroPython_ESP32_psRAM_LoBo/wiki/adc
+#
 # esp32: Vout from microphone/pre-amp to ADC2 ch0 = GPIO4, pin 24.
 #        Note: channel 2 kan alleen worden gebruikt als WiFi is uitgeschakeld.
-
-adc = ADC(Pin(4),unit=2)
-# esp8266
-#adc = ADC(0)
-#adc_c = adc.channel(pin='P34')
-adc_c()
+adc = ADC(Pin(36, Pin.IN), unit = 1) # ADC1, ch0
+# configureer bereik van 0-3.3 V (3.6 max)
+adc.atten(adc.ATTN_11DB)
+# 12 bits resolutie
+adc.width(adc.WIDTH_12BIT)
+print(adc.read())
 print("ADC klaar voor gebruik")
-
-
-while True:
-    v = adc_c.value()
-    print("ADC output = " + str(v))
-
-# zie https://docs.pycom.io/firmwareapi/pycom/machine/adc.html
-adc = machine.ADC()             # create an ADC object
-apin = adc.channel(pin='P16')   # create an analog pin on P16
-val = apin()
-
 
 # (blue) LED on board:
 led = Pin(2, Pin.OUT)
@@ -30,25 +23,30 @@ led = Pin(2, Pin.OUT)
 # Green LED 2  on GPIO17, pin 27.
 # Yellow LED 1 on GPIO18, pin 35.
 # Red LED 1    on GPIO19, pin 38.
-led1 = Pin(16, Pin.OUT)
-led2 = Pin(17, Pin.OUT)
-led3 = Pin(18, Pin.OUT)
-led4 = Pin(19, Pin.OUT)
+led1 = Pin(25, Pin.OUT)
+led2 = Pin(26, Pin.OUT)
+led3 = Pin(27, Pin.OUT)
+led4 = Pin(14, Pin.OUT)
 
 leds = [led1, led2, led3, led4]
 
 while True:
-    randomValue = random.randint(0,4)
     led.value(not led.value())
-    print("randomValue " + str(randomValue))
+    v = adc.read()
+    print("ADC output = " + str(v))
+#    adc.collect(18000 ,len=9000)
+#    v = adc.collected()
+#    print("ADC output = " + str(v))
+    scaledV = int(round(v / 1024))
+    print("scaled output = " + str(scaledV))
     led.value(not led.value())
-    
-    for i in range(0, len(leds)): 
-        if randomValue > i:
-            print("switching on LED number " + str(i))
-            leds[i].value(1)
-        else:
-            print("switching off LED number " + str(i))
-            leds[i].value(0)
-        time.sleep(0.05)
-    time.sleep(0.25)
+#    
+#    for i in range(0, len(leds)): 
+#        if scaledV > i:
+#            print("switching on LED number " + str(i))
+#            leds[i].value(1)
+#        else:
+#            print("switching off LED number " + str(i))
+#            leds[i].value(0)
+#        time.sleep(0.05)
+    time.sleep(1)
